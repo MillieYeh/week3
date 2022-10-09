1fd4e3dfc9d37158412066498092315467e4451f
Millie Yeh <112257871+MillieYeh@users.noreply.github.com>
2022-10-09 14:07:02 +0800

    Update request1.py

--- request1.py
+++ request1.py
@@ -10,22 +10,19 @@ with request.urlopen(src) as response:
 #解讀資料欄位
 list = data["result"]["results"]
 
-for final in list:
-    if final["xpostDate"][3] == "5" or final["xpostDate"][3] == "6":
-        with open("data.csv", "w", encoding="utf-8") as file:
-
-            w = csv.writer(open("data.csv", "w", encoding="utf-8"))
-
-            title = ['景點名稱','區域','經度','緯度','第一張圖檔網址']
-            w.writerow(title)
-
-            for i in list:
-                total_list = []
-                total_list.append(i["stitle"])
-                total_list.append(i["address"][5]+i["address"][6]+i["address"][7])
-                total_list.append(i["longitude"])
-                total_list.append(i["latitude"])
-                total_list.append('https' + i['file'].split('https')[1])
-
-                w.writerow(total_list)
+with open("data.csv", "w", encoding="utf-8") as file:
     
+    w = csv.writer(open("data.csv", "w", encoding="utf-8"))
+    title = ['景點名稱','區域','經度','緯度','第一張圖檔網址']
+    w.writerow(title)
+    
+    for final in list:
+        if final["xpostDate"][3] == "5" or final["xpostDate"][3] == "6":
+            total_list = []
+            total_list.append(final["stitle"])
+            total_list.append(final["address"][5]+final["address"][6]+final["address"][7])
+            total_list.append(final["longitude"])
+            total_list.append(final["latitude"])
+            total_list.append('https' + final['file'].split('https')[1])
+            
+            w.writerow(total_list)
